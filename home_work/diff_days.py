1b32a2ef122434cabd6f2e82a33428a461f22821
Filip-Povidernyi <p.poviderniy@gmail.com>
2025-02-11 13:49:17 +0200

    fix

--- home_work/diff_days.py
+++ home_work/diff_days.py
@@ -2,9 +2,11 @@ from datetime import datetime
 
 
 def get_days_from_today(date: str):
+
     current_day = datetime.now().date()
     got_day = datetime.strptime(date, "%Y-%m-%d").date()
     days_diff = current_day.toordinal() - got_day.toordinal()
+
     return days_diff
 
 
