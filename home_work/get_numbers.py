e5a70056e78376f53a3b1cefe4dadf5d875506fe
Filip-Povidernyi <p.poviderniy@gmail.com>
2025-02-11 13:49:22 +0200

    Update get_numbers.py

--- home_work/get_numbers.py
+++ home_work/get_numbers.py
@@ -4,13 +4,12 @@ import random
 def get_numbers_ticket(min: int, max: int, quantity: int):
 
     numbers = set()
-    count = 0
 
     while len(numbers) < quantity:
         num = random.randint(min, max)
         numbers.add(num)
-        count += 1
-    print(numbers, count)
+
+    return numbers
 
 
 if __name__ == '__main__':
